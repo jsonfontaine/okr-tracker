6c30b8c10b963714ae6a20cf7b1fdeb2014ce82c
Jason Fontaine <Jason.Fontaine@genial.com.vc>
2026-03-17 21:52:15 -0300

    feat(ui): adiciona SnackbarProvider e SnackbarViewport ao componente principal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@genial/design-system/styles';
 import './index.css';
-import { ThemeProvider } from '@genial/design-system';
+import { ThemeProvider, SnackbarProvider, SnackbarViewport } from '@genial/design-system';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider defaultMode="light">
-      <App />
+      <SnackbarProvider>
+        <App />
+        <SnackbarViewport />
+      </SnackbarProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
